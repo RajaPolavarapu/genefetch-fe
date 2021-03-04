2c7bfe8b2cdca9f9ddeda228eb9e00baeb550935
Raja Polavarapu <satyaraj@live.com>
2021-03-04 22:56:12 +0530

    Species and Limit Added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import './App.css';
 import React from "react";
 
 
-const getData = (value) => {
+const getSpecies = () => {
   return new Promise((resolve, reject) => {
-    fetch(`https://genesuggest-emb.herokuapp.com/gene_suggest?limit=&species=homo_sapiens&query=${value}`)
+    fetch(`http://localhost:3001/unique_species`)
+      .then((response) => response.json())
+      .then(result => {
+        if (result && result.data) {
+          resolve(result.data)
+        } else {
+          reject([])
+        }
+      })
+      .catch(err => {
+        reject([])
+      })
+  });
+}
+
+const getData = ({ query, species="homo_sapiens", limit=10 }) => {
+  return new Promise((resolve, reject) => {
+    fetch(`http://localhost:3001/gene_suggest?limit=${limit}&species=${species}&query=${query}`)
       .then((response) => response.json())
       .then(result => {
         if (result && result.data) {
@@ -21,6 +38,10 @@ const getData = (value) => {
 
 const App = () => {
   const [query, setQuery] = React.useState("");
+  const [uniqspecies, setUniqSpecies] = React.useState([]);
+  const [species, setSpecies] = React.useState("homo_sapiens");
+
+  const [limit, setLimit] = React.useState(10)
   const [options, setOptions] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
 
@@ -28,7 +49,7 @@ const App = () => {
     const { value } = e.target;
     setQuery(value);
     setLoading(true);
-    getData(value)
+    getData({ query: value, species, limit })
       .then(response => {
         setOptions(response)
         setLoading(false);
@@ -39,11 +60,36 @@ const App = () => {
       })
   }
 
+  React.useEffect(() => {
+    getSpecies()
+      .then(result => { setUniqSpecies(result) })
+      .catch(err => setUniqSpecies([]))
+  }, [])
+
   return (
     <div className="App" onClick={() => setOptions([])}>
       <header className="App-header">
-        <p>Gene Suggest</p> 
+        <p>Gene Suggest</p>
         <div style={{ position: 'relative' }}>
+          <div className={"selectElements"}>
+            <select className={"Select Select1"} value={species} onChange={(e) => {
+              setQuery("");
+              setSpecies(e.target.value);
+            }} defaultValue={"homo_sapiens"} placeholder="Select Species">
+              {
+                uniqspecies.map((item, index) => <option key={index} value={item}>{item}</option>)
+              }
+            </select>
+            <select className="Select Select2" value={limit} onChange={e => {
+              setQuery("");
+              setLimit(e.target.value)
+            }}>
+              <option value="10">10</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </div>
           <div className="Aligner">
             <input type="text" placeholder="Type something..." className="Autosuggest-text"
               value={query}
@@ -65,9 +111,9 @@ const App = () => {
         </div>
       </header>
       <div className={"footer"}>
-            Name: Raja Polavarapu <br/>
-            Email: satyarajukits@gmail.com<br/>
-            Mobile: +919441841403<br/>
+        Name: Raja Polavarapu <br />
+            Email: satyarajukits@gmail.com<br />
+            Mobile: +919441841403<br />
       </div>
     </div>
   );
